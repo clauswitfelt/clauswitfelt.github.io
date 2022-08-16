4f683fe274609a76e4d8d8e195ac9e7bee1edf68
clauswitfelt <clauswitfelt@gmail.com>
2022-08-16 21:09:17 +0200

    Update script.js

--- nyquiz/script.js
+++ nyquiz/script.js
@@ -1,1 +1,24 @@
+let question = {
+title: 'Hund',
+alternatives: ['dog','cat','horse'],
+  correctAnswer=1
+ 
+};
+
+
+function showQuestion(q)
+{
+
+let titleDiv=document.getElementById('title');
+titleDiv.textcontent=q.title;
+  
+  
+}
+
+showQuestion(question);
+
+
+
+
+
 
